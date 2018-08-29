ffdf92d5c0a4cb2fdc2df0cb7b31bde23009d20a
unknown <jonhake18@gmail.com>
2018-08-29 18:58:41 -0500

    handlebars replaced

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,25 +3,18 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msgHead: "Welcome!",
-        msg:
-          "Thanks for visiting FilmCritic, please signup or login to post your own reviews.",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    //prettier-ignore
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+  // // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   //prettier-ignore
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
